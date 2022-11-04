27d542fd5e5a228ef5b80dd20ca022c02d8515c7
gazinnur <gazinnur@mail.ru>
2022-11-04 14:11:50 +0300

    Решена задача №2 Таблица кубов от 1 до Num

---
+++ Exp002_CubOFn/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+int Cub=-1;
+for (int i = 1; i <= Num; i++)
+{
+    Cub=i*i*i;
+    Console.Write($"{Cub}, ");
+}
